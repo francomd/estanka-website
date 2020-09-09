3b50bcc74a073e2edd88b539202de2f37cc61d09
Franco Dalmasso <francodalmasso@gmail.com>
2020-09-09 00:16:32 -0300

    Add some animations

--- src/javascripts/scripts.js
+++ src/javascripts/scripts.js
@@ -1,4 +1,5 @@
 const $ = require('jquery');
+const SmoothScroll = require('smooth-scroll');
 
 window.$ = $;
 
@@ -22,5 +23,43 @@ document.addEventListener('mousemove', (e) => {
     element.style.webkitTransform = `translateX(${ease}%)`;
     // eslint-disable-next-line no-param-reassign
     element.style.transform = `translateX(${ease}%)`;
+  });
+});
+
+// eslint-disable-next-line no-unused-vars
+const scroll = new SmoothScroll('a[href*="#"]', {
+  ignore: '[data-scroll-ignore]', // Selector for links to ignore (must be a valid CSS selector)
+  header: '.navbar', // Selector for fixed headers (must be a valid CSS selector)
+  topOnEmptyHash: true, // Scroll to the top of the page for links with href="#"
+  speed: 400, // Integer. Amount of time in milliseconds it should take to scroll 1000px
+  speedAsDuration: false, // If true, use speed as the total duration of the scroll animation
+  durationMax: 600, // Integer. The maximum amount of time the scroll animation should take
+  durationMin: 400, // Integer. The minimum amount of time the scroll animation should take
+  clip: true, // If true, adjust scroll distance to prevent abrupt stops near the bottom of the page
+  easing: 'easeInOutCubic', // Easing pattern to use
+  updateURL: true, // Update the URL on scroll
+  popstate: true, // Animate scrolling with the forward/backward browser buttons. Requires updateURL
+});
+
+// Scroll function courtesy of Scott Dowding; http://stackoverflow.com/questions/487073/check-if-element-is-visible-after-scrolling
+
+$(document).ready(() => {
+  // Check if element is scrolled into view
+  function isScrolledIntoView(elem) {
+    const docViewTop = $(window).scrollTop();
+    const docViewBottom = docViewTop + $(window).height();
+
+    const elemTop = $(elem).offset().top;
+    // const elemBottom = elemTop + $(elem).height();
+    // debugger;
+    return elemTop <= (docViewBottom - 150);
+  }
+  // If element is scrolled into view, fade it in
+  $(window).scroll(() => {
+    $('.animate').toArray().forEach((e) => {
+      if (isScrolledIntoView(e) === true) {
+        $(e).addClass('animate__animated animate__fadeInUp');
+      }
+    });
   });
 });
