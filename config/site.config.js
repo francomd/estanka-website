6c323c3fd69b1a551e67b461726ab2ebefbaf370
Franco Dalmasso <francodalmasso@gmail.com>
2020-09-28 23:03:18 -0300

    Apply mobile styles

--- config/site.config.js
+++ config/site.config.js
@@ -15,10 +15,10 @@ const config = {
   site_description: 'Somos una empresa dedicada al diseño, fabricación e instalación de aberturas, cerramientos y estructuras a pedido de aluminio.',
 
   // Your website's URL, used for sitemap
-  site_url: 'https://estanka.com',
+  site_url: 'https://estanka.com.ar',
 
   // Google Analytics tracking ID (leave blank to disable)
-  googleAnalyticsUA: 'UA-173116485-1',
+  googleAnalyticsUA: '',
 
   // The viewport meta tag added to your HTML page's <head> tag
   viewport: 'width=device-width,initial-scale=1',
